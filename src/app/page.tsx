cb528f588ef110bc988931a8645d46a3e4dfc6dd
jam-kt <jamesktorres2022@gmail.com>
2024-11-04 15:15:18 -0800

    feat: homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Navbar from "@/components/Navbar";
+import style from "@/styles/page.module.css";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
       <h1>Home</h1>
+      <div className={style["about"]}>
+        <p>Hi! My name is James and I'm currently a 3rd year Computer Engineer.</p>
+        <p>Please make a merge conflict for me!</p>
+      </div>
     </main>
   );
 }
